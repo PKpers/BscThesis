e83547ad1f094f2c84b04b3acd58413e92cf291b
kpapad02 <dinogreco2000@gmail.com>
2023-05-03 21:16:15 +0300

    did a first training of the WPhiJets_M60M5080 dataset

--- Thesis/Bdt/src/exc/TrainTest.sh
+++ Thesis/Bdt/src/exc/TrainTest.sh
@@ -6,9 +6,9 @@ check_exit(){
     fi
 }
 
-#train="/home/kpapad/UG_thesis/Thesis/Bdt/src/Training.py"
+train="/home/kpapad/UG_thesis/Thesis/Bdt/src/Training.py"
 test="/home/kpapad/UG_thesis/Thesis/Bdt/src/Testing.py"
-dataset="WPhiJets_M200M100300Deltas"
+dataset="WPhiJets_M60M5080Deltas"
 #configs=("training_conf1.dict" "training_conf2.dict" "training_conf3.dict" "training_conf5.dict")
 #"training_conf10.dict" "training_conf11.dict" "training_conf12.dict" "training_conf13.dict" "training_conf14.dict" "training_conf15.dict")
 k=12
@@ -16,9 +16,9 @@ configs=("training_conf"$k".dict")
 for config in "${configs[@]}"; do
     model="myModel"$dataset"_conf"$k
     output=$dataset"PConf"$k"Pred"
-    #python $train $dataset $config
-    #ret=$?
-    #check_exit $ret
+    python $train $dataset $config
+    ret=$?
+    check_exit $ret
     python $test $dataset $model $output
     ret=$?
     check_exit $ret
